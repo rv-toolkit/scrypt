57b1f82cfc741fbaeefa64a80180afee2acf485a
Yiyi Wang <shd101wyy@sina.com>
2018-08-13 15:52:12 -0500

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# This is running under Ubuntu 16.04
+# Install necessary packages. 
+# sudo apt-get install -y libssl-dev
+
+# autoscan
+# aclocal ; autoheader ; autoreconf
+# automake --add-missing
+autoreconf -i
+./configure CC=$compiler LD=$compiler CFLAGS=-fissue-report=`pwd`/my_errors.json
+make -j`nproc`
+make test
+
+# Generate a HTML report with `rv-html-report` command,
+# and output the HTML report to `./report` directory. 
+rv-html-report my_errors.json -o report
+
+# Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
+rv-upload-report `pwd`/report
+
+# Done.
