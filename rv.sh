06ab310117f94f195562f8ecc136a708776c6c16
Yiyi Wang <shd101wyy@sina.com>
2018-08-13 17:22:35 -0500

    .

--- rv.sh
+++ rv.sh
@@ -3,24 +3,21 @@ set -x
 
 # This is running under Ubuntu 16.04
 # Install necessary packages. 
-# sudo apt-get install -y libssl-dev
 sudo apt-get install -y autoconf libssl-dev
 
-# autoscan
-# aclocal ; autoheader ; autoreconf
-# automake --add-missing
+error_file=`pwd`/my_errors.json
 compiler=kcc
 autoreconf -i
-./configure CC=$compiler LD=$compiler CFLAGS=-fissue-report=`pwd`/my_errors.json
+./configure CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file
 make -j`nproc`
 
-rm `pwd`/my_errors.json # remove compile-time errors.
+rm $error_file # remove compile-time errors.
 
 make test
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
-rv-html-report my_errors.json -o report
+rv-html-report $error_file -o report
 
 # Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
 rv-upload-report `pwd`/report
