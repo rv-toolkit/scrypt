93909bd330f7136ee197e7bccf507178d47cb9da
Yiyi Wang <shd101wyy@sina.com>
2018-08-14 15:49:51 -0500

    Update config files

--- rv.sh
+++ rv.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-set -x
+set -x # Print debug info
 
 # This is running under Ubuntu 16.04
 # Install necessary packages. 
 sudo apt-get install -y autoconf libssl-dev
 
 error_file=`pwd`/my_errors.json
-compiler=kcc
 autoreconf -i
-./configure CC=$compiler LD=$compiler CFLAGS=-fissue-report=$error_file
+./configure CC=kcc LD=kcc CFLAGS=-fissue-report=$error_file
 make -j`nproc`
 
-rm $error_file # remove compile-time errors.
+rm $error_file # Remove compile-time errors.
 
-make test
+make test # Run tests
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
