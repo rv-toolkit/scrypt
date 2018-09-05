ef28b677b7fcd273f4bf3adcf99233e7ce779b59
yiyiwang <shd101wyy@sina.com>
2018-09-05 11:10:31 -0500

    Refactor rv.sh

--- rv.sh
+++ rv.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 set -x # Print debug info
 
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+
 # This is running under Ubuntu 16.04
 # Install necessary packages. 
-sudo apt-get install -y autoconf libssl-dev
+apt install -y autoconf libssl-dev
 
-error_file=`pwd`/my_errors.json
 autoreconf -i
-./configure CC=kcc LD=kcc CFLAGS=-fissue-report=$error_file
+./configure CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out"
 make -j`nproc`
 
-rm $error_file # Remove compile-time errors.
+rm $json_out # Remove compile-time errors.
 
 make test # Run tests
 
 # Generate a HTML report with `rv-html-report` command,
 # and output the HTML report to `./report` directory. 
-rv-html-report $error_file -o report
+touch $json_out && rv-html-report $json_out -o $report_out
 
 # Upload your HTML report to RV-Toolkit website with `rv-upload-report` command. 
-rv-upload-report `pwd`/report
+rv-upload-report $report_out
 
 # Done.
